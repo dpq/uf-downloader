05631774f8766e28d67a3abf0eb8bce137c7691f
David Parunakian <jaffar.rumith@gmail.com>
2012-01-13 20:02:21 +0400

    Refactored the code; added the capability to download only the current strip

--- uf-download.py
+++ uf-download.py
@@ -7,15 +7,13 @@ import random
 import httplib
 
 pageurl = 'http://ars.userfriendly.org/cartoons/?id='
-
 fromstr = '<img border="0" src="'
 tostr = '"'
 
-dt = datetime(year=1997, month=11, day=17)
-delta = timedelta(days=1)
+import os
+import os.path
 
-
-while dt < datetime.now():
+def download_comic(dt):
   try:
     id = str(dt.date()).replace("-", "")
     f = urlopen(pageurl + id).read()
@@ -23,14 +21,62 @@ while dt < datetime.now():
     url = f[ifrom: f.find(tostr, ifrom + 1)]
     if url == 'error':
       print("Strip %s (%s) doesn't exist" % (pageurl, str(dt)))
-      dt += delta
-      continue
+      return True
     ext = url[url.rfind("."):]
-    f = open("%s/%s%s" % (dt.year, id, ext), "wb")
+
+    path = "%d" % dt.year
+    if not os.path.exists(path) or (os.path.exists(path) and not os.path.isdir(path)):
+      try:
+        os.mkdir(path)
+      except:
+        print("Could not create target directory, aborting.")
+        exit()
+
+    f = open("%d/%s%s" % (dt.year, id, ext), "wb")
     f.write(urlopen(url).read())
     f.close()
-    time.sleep(6 * random.random())
-    dt += delta
+    return True
   except httplib.IncompleteRead:
-    print("Error while downloading %s (%s)" % (pageurl, str(dt)))
-    continue
+    print("Error while downloading %s (%s). Please retry." % (pageurl, str(dt)))
+    return False
+
+
+def download_archive():
+  dt = datetime(year=1997, month=11, day=17)
+  delta = timedelta(days=1)
+  while dt <= datetime.now():
+    if download_comic(dt):
+      dt += delta
+    # Wait for a while as not to hammer UF servers
+    time.sleep(6 * random.random())
+
+
+def download_current():
+  dt = datetime.now()
+  download_comic(datetime.now())
+
+def show_help():
+  print("""Usage:
+./uf-download.py archive     // Download the WHOLE UserFriendly webcomic archive
+./uf-download.py current     // Download the current comic strip
+./uf-download.py help        // Show this message
+
+Important! Please see the README file for legal hints & other useful information.
+""")
+
+from sys import argv
+
+def main():
+  if len(argv) > 1:
+    if argv[1] == "current":
+      download_current()
+    elif argv[1] == "archive":
+      download_archive()
+    else:
+      show_help()
+  else:
+    show_help()
+
+
+if __name__ == "__main__":
+    main()
